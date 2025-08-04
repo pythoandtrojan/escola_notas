565214e706630e1efb6e04c9dec643df725547b6
pythoandtrojan <erik.silva40@aluno.educa.go.gov.br>
2025-08-04 01:04:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import os 
 
+def clear():
+    os.system('clear')
+
+def cadastro():
+    while True:
+        try:
+            print("1. entrar")
+            print("2. admin")
+            print("3. sair")
+            escolha = int(iput("escolha: "))
+            if escolha == 1:
+                
